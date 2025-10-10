33b0fe6774554cb436386744875f0415b219949a
milanhorvath <35398860+milanhorvath@users.noreply.github.com>
2025-10-10 14:44:18 +0200

    Update entrypoint.sh to alpine linux scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,82 +1,81 @@
+#!/bin/sh
 echo "Starting..."
-if [[ -v ONECX_VAR_REMAP ]]; then
-  echo "Re-mapping env vars: ${ONECX_VAR_REMAP}"
-  IFS=";"
-  for pair in ${ONECX_VAR_REMAP}; do
-    key=${pair%=*}
-    val=${pair#*=}
-    echo "Create var: '$key' from var '$val'"
-    export "${key}"="${!val}"
-  done
-  echo "Re-mapping finished"
-fi 
-unset IFS
 
+# Re-map environment variables
+if [ -n "$ONECX_VAR_REMAP" ]; then
+  echo "Re-mapping env vars: ${ONECX_VAR_REMAP}"
+  OLD_IFS="$IFS"
+  IFS=";"
+  for pair in $ONECX_VAR_REMAP; do
+    key=$(echo "$pair" | cut -d= -f1)
+    val=$(echo "$pair" | cut -d= -f2)
+    echo "Create var: '$key' from var '$val'"
+    eval "export ${key}=\"\${$val}\""
+  done
+  IFS="$OLD_IFS"
+  echo "Re-mapping finished"
+fi
 
 # Check custom env.json
 if [ -f /tmp/env.json ]; then
   echo "Copying /tmp/env.json -> ${DIR_ASSETS}/env.json"
-  cp /tmp/env.json ${DIR_ASSETS}/env.json
+  cp /tmp/env.json "${DIR_ASSETS}/env.json"
 else
   echo "No custom ${DIR_ASSETS}/env.json configuration"
 fi
 
-IFS=, read -ra config_env_names <<< "$CONFIG_ENV_LIST"
-IFS=, read -ra config_env_ext_names <<< "$CONFIG_ENV_EXT_LIST"
-config_env_names=( "${config_env_names[@]}" "${config_env_ext_names[@]}" )
+# Merge config env lists
+config_env_names=$(echo "$CONFIG_ENV_LIST,$CONFIG_ENV_EXT_LIST" | tr ',' ' ')
 
 # Update environment variables in env.json
-if [ -f ${DIR_ASSETS}/env.json ]; then
-  
-  for e in $(jq -r 'to_entries[]|.value' ${DIR_ASSETS}/env.json); do
-    if [[ $e == \$\{*\} ]]; then
-      e=${e:2}
-      config_env_names=$config_env_names" "${e::-1}
-    fi
+if [ -f "${DIR_ASSETS}/env.json" ]; then
+  for e in $(jq -r 'to_entries[]|.value' "${DIR_ASSETS}/env.json"); do
+    case "$e" in
+      '${'*'}')
+        varname=$(echo "$e" | sed -e 's/^${//' -e 's/}$//')
+        config_env_names="$config_env_names $varname"
+        ;;
+    esac
   done
 
   echo "Updating ${DIR_ASSETS}/env.json"
-  for item in ${config_env_names[@]}; do
-    value=$(printf '%s\n' "${!item}")
-    if [[ ! -z "$value" ]]; then
+  for item in $config_env_names; do
+    value=$(eval "printf '%s' \"\${$item}\"")
+    if [ -n "$value" ]; then
       echo "Replace '$item' with '$value' in env.json"
-      sed -i "s|\${$item}|$value|g" ${DIR_ASSETS}/env.json
+      sed -i "s|\${$item}|$value|g" "${DIR_ASSETS}/env.json"
     fi
   done
 
-  if [[ ${ADDITIONAL_REPLACE:-false} == true ]]; then
+  if [ "${ADDITIONAL_REPLACE:-false}" = "true" ]; then
     echo "Second update ${DIR_ASSETS}/env.json"
-    for item in ${config_env_names[@]}; do
-      value=$(printf '%s\n' "${!item}")
-      if [[ ! -z "$value" ]]; then
+    for item in $config_env_names; do
+      value=$(eval "printf '%s' \"\${$item}\"")
+      if [ -n "$value" ]; then
         echo "Replace '$item' with '$value' in env.json"
-        sed -i "s|\${$item}|$value|g" ${DIR_ASSETS}/env.json
+        sed -i "s|\${$item}|$value|g" "${DIR_ASSETS}/env.json"
       fi
     done
   fi
 
   # Create INJECTED_ENV from env.json
-  envJsonAsString=$(sed -E 's/\$\{.*\}/@UNDEFINED/' ${DIR_ASSETS}/env.json | tr -d "\n")
+  envJsonAsString=$(sed -E 's/\$\{.*\}/@UNDEFINED/' "${DIR_ASSETS}/env.json" | tr -d "\n")
   escaped_conf=$(printf '%s\n' "$envJsonAsString" | sed -e 's/[\/&]/\\&/g')
   injectScript="window[\"APP_CONFIG\"]=$escaped_conf;"
-  export INJECTED_ENV=$injectScript
-  echo "Create and add 'INJECTED_ENV' generate variable from ${DIR_ASSETS}/env.json to CONFIG_ENV list of variables"
-  config_env_names=$config_env_names" INJECTED_ENV"
+  export INJECTED_ENV="$injectScript"
+  echo "Create and add 'INJECTED_ENV' to CONFIG_ENV list of variables"
+  config_env_names="$config_env_names INJECTED_ENV"
 else
-  echo "Missing ${DIR_ASSETS}/env.json file to replace 'INJECTED_ENV'"  
+  echo "Missing ${DIR_ASSETS}/env.json file to replace 'INJECTED_ENV'"
 fi
 
-
-
-# Update environment variables in the nginx configuration files
+# Update environment variables in nginx config files
 for file in ${DIR_SERVER_BLOCKS}/*.conf ${DIR_LOCATION}/*.conf; do
   echo "Replacing '@@<var>' placeholders in file: $file"
-  for item in ${config_env_names[@]}; do
-    value=$(printf '%s\n' "${!item}")
-    # echo "Replace '$item' in $file"
-    sed -i "s|@@$item|$value|g" $file
+  for item in $config_env_names; do
+    value=$(eval "printf '%s' \"\${$item}\"")
+    sed -i "s|@@$item|$value|g" "$file"
   done
 done
 
 echo "Done"
-exec "$@"
